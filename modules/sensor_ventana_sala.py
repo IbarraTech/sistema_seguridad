ebd0d795362725718ee34691d3698d05842ede19
IbarraTech <oscaribarracardona@gmail.com>
2023-12-09 09:22:04 -0500

    fix: se cambia la logica del archivo sensor ventana sala, para correccion de errores 4.0

--- modules/sensor_ventana_sala.py
+++ modules/sensor_ventana_sala.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-import modulos.envio_correos as envio_correos
+import modules.envio_correos as envio_correos
 
 
 def detectar_ventana():
@@ -10,34 +10,29 @@ def detectar_ventana():
 
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(pulsador_encendido_pin, GPIO.IN)
-    GPIO.setup(pulsador_apagado_pin, GPIO.IN)
+    GPIO.setup(pulsador_encendido_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(pulsador_apagado_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(led_pin, GPIO.OUT)
+
+    # Asegurarse de que el LED comienza en estado apagado
+    GPIO.output(led_pin, GPIO.LOW)
 
     led_encendido = False
 
     try:
         while True:
-            if GPIO.input(pulsador_encendido_pin) == GPIO.HIGH and not led_encendido:
+            if GPIO.input(pulsador_encendido_pin) == GPIO.HIGH:
                 GPIO.output(led_pin, GPIO.HIGH)
                 led_encendido = True
-                while GPIO.input(pulsador_apagado_pin) == GPIO.LOW:
-                    pass
-                GPIO.output(led_pin, GPIO.LOW)
-                led_encendido = False
                 time.sleep(0.1)
-                envio_correos()
+                envio_correos.envio_correos()
 
-                # Llamar a la función de notificación
-                # envio_correos(remitente, destinatario, mensaje_correo)
-
-            if GPIO.input(pulsador_apagado_pin) == GPIO.HIGH and led_encendido:
+            if GPIO.input(pulsador_apagado_pin) == GPIO.HIGH:
                 GPIO.output(led_pin, GPIO.LOW)
                 led_encendido = False
 
     except KeyboardInterrupt:
         GPIO.cleanup()
-        detectar_ventana()
 
 
 detectar_ventana()
