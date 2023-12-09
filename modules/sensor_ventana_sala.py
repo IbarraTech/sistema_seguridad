eada53e4cca2827caa3f7ac42c50e77350f6c7a4
IbarraTech <oscaribarracardona@gmail.com>
2023-12-09 09:12:12 -0500

    fix: se cambia la logica del archivo sensor ventana sala, para correccion de errores 3.0

--- modules/sensor_ventana_sala.py
+++ modules/sensor_ventana_sala.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+""" import RPi.GPIO as GPIO
 import time
 import modulos.envio_correos as envio_correos
 
@@ -38,6 +38,42 @@ def detectar_ventana():
     except KeyboardInterrupt:
         GPIO.cleanup()
         detectar_ventana()
+
+
+detectar_ventana() """
+
+import RPi.GPIO as GPIO
+import time
+import modules.envio_correos as envio_correos
+
+
+def detectar_ventana():
+    led_pin = 11
+    pulsador_encendido_pin = 7
+    pulsador_apagado_pin = 18
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pulsador_encendido_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(pulsador_apagado_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(led_pin, GPIO.OUT)
+
+    led_encendido = False
+
+    try:
+        while True:
+            if GPIO.input(pulsador_encendido_pin) == GPIO.HIGH:
+                GPIO.output(led_pin, GPIO.HIGH)
+                led_encendido = True
+                time.sleep(0.1)
+                envio_correos.envio_correos()
+
+            if GPIO.input(pulsador_apagado_pin) == GPIO.HIGH:
+                GPIO.output(led_pin, GPIO.LOW)
+                led_encendido = False
+
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
 
 detectar_ventana()
