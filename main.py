7a66d986b7e67143aedab057553f640a2378f323
IbarraTech <oscaribarracardona@gmail.com>
2023-12-09 11:42:42 -0500

    feature: se crea modulo para el sensor ventana de la habitacion y se crea las lineas de codigo en archivo main, para el funcionamiento

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import modules.sensor_ventana_sala as sensor_ventana_sala
+import modules.sensor_ventana_habitacion as sensor_ventana_habitacion
 
 
 # Llamar a la función del sensor de ventana
 sensor_ventana_sala.detectar_ventana()
+sensor_ventana_habitacion.detectar_habitacion()
